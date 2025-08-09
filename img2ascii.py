f802312c5130e853e11fd2bdcbcf6a55eb959388
cnrabaya <cnlrabaya@gmail.com>
2025-08-09 18:48:45 +0800

    Added image compression

--- img2ascii.py
+++ img2ascii.py
@@ -1,41 +1,46 @@
 from PIL import Image
-import os
 
-def img2ascii(filename):
-    img = Image.open(filename)
+# Compresses image for high resolution images
+def ResizeImage(img, new_width = 200):
     width, height = img.size
+    aspect_ratio = height / width
+
+    # Makes new height smaller to compensate for height of ASCII characters
+    compensation_factor = 0.55 
+    new_height = int(aspect_ratio * new_width * compensation_factor) 
+
+    return img.resize((new_width, new_height))
+
+def Img2ASCII(filename, isCompressed):
+    # ASCII characters used:
+    chars = """ .-':_,=;+!)]}0X#%$@"""
+
+    # List is reversed so that lower index = lower brightness
+    reversed_chars = chars[::-1]
+    ASCII_dict = {i: ch for i, ch in enumerate(reversed_chars)}
+    
+    img = Image.open(filename)
+    
+    if isCompressed.lower() == 'y':
+        new_width = int(input("Enter new width (px): "))
+        img = ResizeImage(img, new_width)
+    width, height = img.size
+
+    # Output file name:
     f = open(filename + "_ascii.txt", "w")
 
+    # Iterates through each pixel, converts RGB value to intensity and writes equivalent ASCII value
     for y in range(height):
         for x in range(width):
             pixel = img.getpixel((x,y))
             r, g, b = pixel[:3]
             rel_brightness = (0.299*r + 0.587*g + 0.114*b) / 255
-            
-            if (rel_brightness > 0.9):
-                f.write(" ")
-            elif (rel_brightness > 0.8 and rel_brightness <= 0.9):
-                f.write(".")
-            elif (rel_brightness > 0.7 and rel_brightness <= 0.8):
-                f.write("'")
-            elif (rel_brightness > 0.6 and rel_brightness <= 0.7):
-                f.write("-")
-            elif (rel_brightness > 0.5 and rel_brightness <= 0.6):
-                f.write("-")
-            elif (rel_brightness > 0.4 and rel_brightness <= 0.5):
-                f.write("+")
-            elif (rel_brightness > 0.3 and rel_brightness <= 0.4):
-                f.write("*")
-            elif (rel_brightness > 0.2 and rel_brightness <= 0.3):
-                f.write("%")
-            elif (rel_brightness > 0.1 and rel_brightness <= 0.2):
-                f.write("$")
-            else:
-                f.write("#")
-            
+            ascii_index = round(rel_brightness*(len(ASCII_dict)-1))
+            f.write(ASCII_dict[ascii_index])
         f.write("\n")
-    
     f.close()
+ 
+    print("Image converted: ", filename + "_ascii.txt")
     return
 
 def main():
@@ -44,8 +49,8 @@ def main():
         filename = input("Enter the filename: ")
         if (filename == "exit"):
             break
-        
-    img2ascii(filename)
+    isCompressed = input("Compress Image? [Y or N]: ")  
+    Img2ASCII(filename, isCompressed)
 
 if __name__=="__main__":
     main()
